d865f2e8f195c25a64c4e9473659dcec4dc66d8d
Jake Metz <JakeMetz1@outlook.com>
2019-06-06 00:26:33 +1000

    add breakpoint styling to AppNavigation

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,9 +1,23 @@
 <template>
-  <div></div>
+  <v-toolbar app color="teal" dark>
+    <v-toolbar-side-icon
+      class="hidden-sm-and-up"
+      @click="drawer = !drawer"
+    ></v-toolbar-side-icon>
+    <v-spacer class="hidden-sm-and-up"></v-spacer>
+    <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+    <v-spacer class="hidden-xs-only"></v-spacer>
+    <v-btn class="hidden-xs-only" flat>SIGN IN</v-btn>
+  </v-toolbar>
 </template>
 <script>
 export default {
-  name: 'AppNavigation'
+  name: 'AppNavigation',
+  data() {
+    return {
+      appTitle: 'Vinnie App'
+    };
+  }
 };
 </script>
-<style></style>
+<style scoped></style>
